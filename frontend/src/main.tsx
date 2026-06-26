12192de3a1e9abdd955f127507d3e624959ea09d
jhayniffy <jeremiahniffypeter@gmail.com>
2026-06-26 22:44:36 +0100

    feat: add global toast notification system with auto-dismiss and manual close

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,14 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { SettlementApproval } from './components/SettlementApproval'
+import { ToastProvider } from './components/Toast'
+import './components/Toast.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/settlements" element={<SettlementApproval />} />
-        <Route path="*" element={<Navigate to="/settlements" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <ToastProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/settlements" element={<SettlementApproval />} />
+          <Route path="*" element={<Navigate to="/settlements" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </ToastProvider>
   </React.StrictMode>,
 )
